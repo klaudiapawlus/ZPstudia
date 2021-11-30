ce102782b14f86b5bfdd929a176641a5d04a600a
klaudiapawlus <93867783+klaudiapawlus@users.noreply.github.com>
2021-11-30 22:46:32 +0100

    Delete zad_6.py

--- zad_6.py
+++
@@ -1,9 +0,0 @@
-def zad6 (lista1:list,lista2: list ) -> list:
-    lista3 = lista1 + lista2
-    lista3=list(dict.fromkeys(lista3))
-
-
-    return [l **3  for l in  lista3]
-
-
-print (zad6([1,2,3,4],[6,6,5,9]))
